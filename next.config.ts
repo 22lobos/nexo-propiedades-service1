805c8c578787a4fc32a136f83e481c0691473bcd
22lobos <cr.loboso@duocuc.cl>
2026-06-08 20:22:37 -0400

    fix: next config para app router en Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     appDir: true,
   },
-};
-export default nextConfig;
+  output: 'standalone',
+}
+
+module.exports = nextConfig
